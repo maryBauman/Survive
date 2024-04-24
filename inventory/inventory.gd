e3bb90b3db71f52fe1a6d07562ef83e0ace69300
Mary <marybauman503@gmail.com>
2024-04-24 17:31:00 -0400

    inv slots

--- inventory/inventory.gd
+++ inventory/inventory.gd
@@ -1,6 +1,11 @@
 extends Resource
 class_name Inventory
+
+signal update
+
 @export var slots: Array[InvSlot]
 
 func insert(item: Inventory_item):
-	pass
+	var item_slots = slots.filter(func(slot): return slot.item==item)
+	if !item_slots.is_empty():
+		item_slots[0].amount+=1
