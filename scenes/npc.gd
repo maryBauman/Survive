2bc681e3be5f3b6fb97f9b7da33b059fa81ff124
Mary <marybauman503@gmail.com>
2024-06-04 19:23:43 -0400

    npc

--- scenes/npc.gd
+++ scenes/npc.gd
@@ -19,3 +19,25 @@ enum {
 func _ready():
 	randomize()
 	start_pos = position
+	
+func _process(delta):
+	if cur_state==0 or cur_state==1:
+		$AnimatedSprite2D.play("idle")
+	elif cur_state==2 and !is_chatting:
+		if dir.y == -1:
+			$AnimatedSprite2D.play("n-walk")
+		elif dir.x == 1:
+			$AnimatedSprite2D.play("e-walk")
+		elif dir.y == 1:
+			$AnimatedSprite2D.play("s-walk")
+		elif dir.x == -1:
+			$AnimatedSprite2D.play("w-walk")
+#		elif dir.x > 0.5 and dir.y < 0.5:
+#			$AnimatedSprite2D.play("ne-walk")
+#		elif dir.x > 0.5 and dir.y > 0.5:
+#			$AnimatedSprite2D.play("se-walk")
+#		elif dir.x < 0.5 and dir.y < 0.5:
+#			$AnimatedSprite2D.play("nw-walk")
+#		elif dir.x < 0.5 and dir.y > 0.5:
+#			$AnimatedSprite2D.play("sw-walk")
+		
