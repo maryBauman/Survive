3217ccce2d3b0f910294055eff8092092689dfad
Mary <marybauman503@gmail.com>
2024-08-27 21:38:02 -0400

    9:10

--- scenes/npc_1_quests.gd
+++ scenes/npc_1_quests.gd
@@ -1,11 +1,29 @@
 extends Control
 
+signal quest_menu_closed
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var quest1_active = false
+var quest1_completed = false
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+#quest 1
+var stickCount = 0
+func quest_chat_1():
+	$questUI.visible = true
+func yes_pressed_1():
+	$questUI.visible = false
+	quest1_active = true
+	stick = 0
+	emit_signal(quest_menu_closed)
+func no_pressed_1():
+	$questUI.visible = false
+	quest1_active = false
+	emit_signal(quest_menu_closed)
+	
+	
+	
 func _process(delta):
-	pass
+	if quest1_active:
+		if stick>=3:
+			print("quest 1 completed")
+			quest1_active = false
+			quest1_completed = true
